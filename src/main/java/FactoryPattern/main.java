77db453542c587e11ceb60d259cae93afeeeacc0
tianhong <tianhong229@gmail.com>
2018-10-11 15:00:02 +0800

    added factory pattern

---
+++ src/main/java/FactoryPattern/main.java
@@ -0,0 +1,20 @@
+package FactoryPattern;
+
+// 创建对象不用向客户端暴露创建逻辑， 通过一个公共接口指向新创建的对象
+public class main {
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+        //获取 Circle 的对象，并调用它的 draw 方法
+        Shape shape1 = shapeFactory.getShape("CIRCLE");
+        //调用 Circle 的 draw 方法
+        shape1.draw();
+        //获取 Rectangle 的对象，并调用它的 draw 方法
+        Shape shape2 = shapeFactory.getShape("RECTANGLE");
+        //调用 Rectangle 的 draw 方法
+        shape2.draw();
+        //获取 Square 的对象，并调用它的 draw 方法
+        Shape shape3 = shapeFactory.getShape("SQUARE");
+        //调用 Square 的 draw 方法
+        shape3.draw();
+    }
+}
